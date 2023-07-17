466d6a3e3a5e117d06112737382ce3d49e1d92e1
Milos Zivlak <zivlakmilos@gmail.com>
2023-07-17 06:03:20 +0200

    Create and open project

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,50 @@
 import { Component } from "solid-js";
 import logo from '../logo.svg';
 import { useNavigate } from "@solidjs/router";
+import { project, setProject, createNewProject, Project, parseProject } from "../store/ProjectStore";
 
 const Home: Component = () => {
   const navigate = useNavigate();
 
+  let fileInputRef: HTMLInputElement | undefined;
+
   const createProject = () => {
+    setProject(createNewProject());
     navigate('/project');
   }
 
   const openProject = () => {
-    navigate('/project');
+    if (!fileInputRef || !fileInputRef.files || !fileInputRef.files.length) {
+      return;
+    }
+
+    const fileReader = new FileReader();
+    fileReader.addEventListener('load', (event: ProgressEvent<FileReader>) => {
+      if (!event.target) {
+        return;
+      }
+
+      const data = event.target.result as string;
+      const project = parseProject(data);
+      setProject(project);
+      navigate('/project');
+    });
+    fileReader.readAsText(fileInputRef.files[0]);
+
+  }
+
+  const openProjectFile = () => {
+    if (!fileInputRef) {
+      return;
+    }
+
+    fileInputRef.click();
   }
 
   return (
     <div class="grid h-screen place-items-center items-center">
+      <input type="file" hidden ref={fileInputRef} accept="application/json" onChange={openProject} />
+
       <div class="flex flex-col w-full lg:flex-row place-content-center">
         <div class="card w-96 bg-base-100 shadow-xl">
           <figure><img src={logo} alt="Shoes" /></figure>
@@ -35,7 +65,7 @@ const Home: Component = () => {
             <h2 class="card-title">Open Project!</h2>
             <p>Open existing SonataJS project?</p>
             <div class="card-actions justify-end">
-              <button class="btn btn-primary" onClick={openProject}>Open Project</button>
+              <button class="btn btn-primary" onClick={openProjectFile}>Open Project</button>
             </div>
           </div>
         </div>
---
+++ src/store/ProjectStore.ts
@@ -0,0 +1,58 @@
+import { createStore } from 'solid-js/store';
+
+export type ProjectSecurity = {
+  select: string,
+  insert: string,
+  update: string,
+  delete: string,
+}
+
+export type ProjectCascade = {
+  update: boolean,
+  delete: boolean,
+}
+
+export type ProjectField = {
+  id: string,
+  type: string,
+  primary_key?: boolean,
+  not_null?: boolean,
+  default_value: string,
+  title: string,
+  relation?: string,
+  cascade?: ProjectCascade,
+  read_only?: boolean,
+  hidden?: boolean,
+}
+
+export type ProjectEntity = {
+  id: string,
+  title: string,
+  row_level_security?: boolean,
+  security?: ProjectSecurity,
+  fields: ProjectField[],
+}
+
+export type Project = {
+  backend: string,
+  frontned: string,
+  ui: string,
+  data: ProjectEntity[],
+}
+
+export const createNewProject = (): Project => {
+  return {
+    backend: 'supabase',
+    frontned: 'solidjs',
+    ui: 'daisyui',
+    data: [],
+  }
+}
+
+export const parseProject = (data: string): Project => {
+  const json = JSON.parse(data);
+  const project = json as Project;
+  return project;
+}
+
+export const [project, setProject] = createStore<Project>(createNewProject());
